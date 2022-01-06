b39605d05211a4ecf4aeb2c502e1aa74eadfdf61
Dev-John06 <66971770+Dev-John06@users.noreply.github.com>
2022-01-06 22:45:18 +0000

    Pisscolour added

--- index.js
+++ index.js
@@ -12,4 +12,40 @@ function HasPiss(text) {
     }
 }
 
+function ColourPiss(color) {
+    // switch statement for each colour console log in a color.
+    switch (color) {
+        case "red":
+            console.log(chalk.red("Piss"));
+            break;
+        case "green":
+            console.log(chalk.green("Piss"));
+            break;
+        case "blue":
+            console.log(chalk.blue("Piss"));
+            break;
+        case "yellow":
+            console.log(chalk.yellow("Piss"));
+            break;
+        case "magenta":
+            console.log(chalk.magenta("Piss"));
+            break;
+        case "cyan":
+            console.log(chalk.cyan("Piss"));
+            break;
+        case "white":
+            console.log(chalk.white("Piss"));
+            break;
+        case "gray":
+            console.log(chalk.gray("Piss"));
+            break;
+        case "black":
+            console.log(chalk.black("Piss"));
+            break;
+        default:
+            console.error("Error: Invalid color");
+            break;
+    }
+}
+
 module.exports = { Piss, HasPiss }
